59e80e67e98ae57101d31b408c56a95a21286aa0
nsvincent <132816219+nsvincent@users.noreply.github.com>
2023-05-16 20:13:02 +0300

    Dataframes ok, missing data binding and callbacks

--- HappinessReportNotebook.py
+++ HappinessReportNotebook.py
@@ -232,12 +232,26 @@ Y2021Df = pd.merge(happinessDataDf.query('Time_Code=="YR2021"'), Y2021ScoresDf, 
 dataArray = [Y2015Df, Y2016Df,Y2017Df,Y2018Df,Y2019Df,Y2020Df,Y2021Df]
 mergedData = pd.concat(dataArray)
 
+#same treatment for scores
+dataArrayScoresdetDf = [Y2015ScoresdetDf, 
+                        Y2016ScoresdetDf, 
+                        Y2017ScoresdetDf, 
+                        Y2018ScoresdetDf, 
+                        Y2019ScoresdetDf, 
+                        Y2020ScoresdetDf, 
+                        Y2021ScoresdetDf]
+mergedScoresdetDfData = pd.concat(dataArrayScoresdetDf)
+
 
 # In[12]:
 
 
 # create sample chart from data
-fig = px.scatter(mergedData, x="Life_expectancy_at_birth,_total_(years)", y="GDP_per_capita,_PPP_(current_international_$)", animation_frame="Time", hover_name="Country_Name" )
+fig = px.scatter(mergedData, 
+                 x="Life_expectancy_at_birth,_total_(years)", 
+                 y="GDP_per_capita,_PPP_(current_international_$)", 
+                 animation_frame="Time", 
+                 hover_name="Country_Name" )
 fig.show()
 
 
@@ -248,7 +262,6 @@ fig.show()
 fig1 = px.choropleth(mergedData, 
                     locations="Country_Code",
                     hover_name="Country_Name",
-                    # hover_data="Country_Code",
                     color="Happiness_Score",
                     color_continuous_scale=px.colors.sequential.Plasma)
 fig1.show()
@@ -258,42 +271,92 @@ fig1.show()
 
 
 geospatialVisual = html.Div([
-                        dcc.Graph(
-                            id='geospatial',
-                        )
-                    ], style={'display': 'inline-block', 'padding': '0 20'})
+    dcc.Graph(
+    id='geospatial', 
+    hoverData={'points': [{'hovertext': 'Finland'}]}
+    )
+    ], style={'padding': '0 20'})
 
 
 # In[ ]:
 
 
 happinessScoreVisual = html.Div([
-                            dcc.Graph(
-                                id='happiness-score-scatter-line'
-                            )
-                        ], style={ 'display': 'inline-block', 'padding': '0 20'})
+    dcc.Graph(
+    id='happiness-score-scatter-line'
+    )
+    ], style={'padding': '0 20'})
 
 # In[ ]:
 
 
-happinessScoreVisual = html.Div([
+happinessScoreDetailVisual = html.Div([
                             dcc.Graph(
                                 id='happiness-score-detail-polarchart'
                             )
-                        ], style={ 'display': 'inline-block', 'padding': '0 20'})
+                        ], style={'padding': '0 20'})
 
 
 # In[15]:
 
 
+app.title = "World Happiness Report Visualization"
 app.layout = html.Div([
     html.Div([
-        geospatialVisual,
-        happinessScoreVisual,
+        html.H1("World Happiness Visualization"),
+        html.Div([
+            html.Div([
+                dcc.Dropdown(
+                    options={
+                        'GDP_per_capita_growth_(annual_%)#CO2_emissions_(metric_tons_per_capita)': 'Economic Production',
+                        'Life_expectancy_at_birth,_female_(years)#Life_expectancy_at_birth,_male_(years)': 'Health',
+                        # 'Adjusted_savings:_carbon_dioxide_damage_(%_of_GNI)#CPIA_social_protection_rating_(1=low_to_6=high)':'Social Support',
+                        # 'Intentional_homicides_(per_100,000_people)#Internally_displaced_persons,_total_displaced_by_conflict_and_violence_(number_of_people)':'Human Rights',
+                        # 'Proportion_of_people_living_below_50_percent_of_median_income_(%)#Multidimensional_poverty_headcount_ratio_(%_of_total_population)':'Charity',
+                        # 'CPIA_transparency,_accountability,_and_corruption_in_the_public_sector_rating_(1=low_to_6=high)#CPIA_policy_and_institutions_for_environmental_sustainability_rating_(1=low_to_6=high)':'Corruption'
+                    },
+                    value='GDP_per_capita_growth_(annual_%)#CO2_emissions_(metric_tons_per_capita)',
+                    id='crossfilter-yaxis-column',
+                    placeholder="Select an indicator category"
+                ),
+            ], style={'width': '100%'})
+        ], style={
+            'width': '100%',
+            'padding': '10px 5px'
+        }),
     ], style={
-        'width': '50%',
+        'display': 'flex',
+        'width': '100%',
         'padding': '10px 5px'
     }),
+    html.Div([
+        html.Div([
+            geospatialVisual,
+            happinessScoreVisual,
+            happinessScoreDetailVisual
+        ], style={
+            'width': '69%',
+            'padding': '10px 5px',
+
+        }),
+        html.Div([
+            dcc.Graph(id='indicator-series1'),
+            dcc.Graph(id='indicator-series2'),
+        ], style={'width': '29%'}),
+    ], style={
+        'display': 'flex',
+        'width': '100%',
+        'padding': '10px 5px'
+    }),
+
+
+    html.Div(dcc.Slider(
+        min=mergedData['Time'].min(),
+        max=mergedData['Time'].max(),
+        id='year-slider',
+        value=mergedData['Time'].max(),
+        marks={str(time): str(time) for time in mergedData['Time'].unique()}
+    ), style={'width': '97%', 'padding': '10px 20px 20px 20px'}),
     # html.Div([
     #     dcc.Graph(id='variable1-scatter-line'),
     #     dcc.Graph(id='variable2-scatter-line'),
@@ -306,14 +369,9 @@ app.layout = html.Div([
     #         """),
     #         html.Pre(id='hover-data')
     #     ], className='three columns'),
-    html.Div(dcc.Slider(
-        min=mergedData['Time'].min(),
-        max=mergedData['Time'].max(),
-        id='year-slider',
-        value=mergedData['Time'].max(),
-        marks={str(time): str(time) for time in mergedData['Time'].unique()}
-    ), style={'width': '97%', 'padding': '10px 20px 20px 20px'}),
-])
+], style={
+    'height': '100vh'
+})
 
 
 # In[ ]:
@@ -323,12 +381,12 @@ app.layout = html.Div([
     Output('geospatial', 'figure'),
     Input('year-slider', 'value'))
 def update_graph(year_value):
-    df=mergedData[mergedData['Time'] == year_value]
-    fig =  px.choropleth(df, 
-                    locations="Country_Code",
-                    hover_name="Country_Name",
-                    color="Happiness_Score",
-                    color_continuous_scale=px.colors.sequential.Plasma)
+    df = mergedData[mergedData['Time'] == year_value]
+    fig = px.choropleth(df,
+                        locations="Country_Code",
+                        hover_name="Country_Name",
+                        color="Happiness_Score",
+                        color_continuous_scale=px.colors.sequential.Plasma)
 
     return fig
 
@@ -340,15 +398,65 @@ def update_graph(year_value):
 @app.callback(
     Output('happiness-score-scatter-line', 'figure'),
     Input('geospatial', 'hoverData'))
-def update_graph(HoverData):
-    countryName = HoverData['points'][0]['hovertext']
-    df=mergedData[mergedData['Country_Name'] == countryName]
-    fig =  px.scatter(df, 
-                      x="Time", 
-                      y="Happiness_Score",
-                      hover_name="Country_Name" )
+def update_graph(geoHoverData):
+    countryName = geoHoverData['points'][0]['hovertext']
+    df = mergedData[mergedData['Country_Name'] == countryName]
+    fig = px.scatter(df,
+                     x="Time",
+                     y="Happiness_Score",
+                     hover_name="Country_Name")
     fig.update_traces(mode='lines+markers')
+
     return fig
+
+
+def create_time_series(df, axis_column, title):
+    titleTimeSeries = title.replace('_', ' ')
+    fig = px.scatter(df, x='Time', y=axis_column)
+
+    fig.update_traces(mode='lines+markers')
+
+    fig.update_xaxes(showgrid=False)
+
+    fig.add_annotation(x=0, y=0.85, xanchor='left', yanchor='bottom',
+                       xref='paper', yref='paper', showarrow=False, align='left',
+                       text=titleTimeSeries)
+
+    fig.update_layout(height=250, margin={'l': 20, 'b': 30, 'r': 10, 't': 10})
+
+    return fig
+
+
+@app.callback(
+    Output('indicator-series1', 'figure'),
+    Input('geospatial', 'hoverData'),
+    Input('crossfilter-yaxis-column', 'value'))
+def update_indicator_series1(geoHoverData, value):
+    countryName = geoHoverData['points'][0]['hovertext']
+    df = mergedData[mergedData['Country_Name'] == countryName]
+    y_columns = value.split("#")
+    indicator1 = y_columns[0]
+    title = '<b>{}</b><br>{}'.format(countryName, indicator1)
+    return create_time_series(df, indicator1, title)
+
+
+@app.callback(
+    Output('indicator-series2', 'figure'),
+    Input('geospatial', 'hoverData'),
+    Input('crossfilter-yaxis-column', 'value'))
+def update_indicator_series2(geoHoverData, value):
+    countryName = geoHoverData['points'][0]['hovertext']
+    df = mergedData[mergedData['Country_Name'] == countryName]
+    y_columns = value.split("#")
+    indicator2 = y_columns[1]
+    title = '<b>{}</b><br>{}'.format(countryName, indicator2)
+    return create_time_series(df, indicator2, title)
+# logging
+# @app.callback(
+#     Output('hover-data', 'children'),
+#     Input('geospatial', 'hoverData'))
+# def display_hover_data(hoverData):
+#     return json.dumps(hoverData, indent=2)
 
 # In[17]:
 # logging
@@ -360,12 +468,46 @@ def update_graph(HoverData):
 
 # In[18]:
 
-## WIP from there and below !!
+## WIP !!
 
-values = mergedData[(mergedData['Country_Name'] == "Finland")
-                  & (mergedData['Time'] == 2021)]
+# values = mergedData[(mergedData['Country_Name'] == "Finland")
+#                   & (mergedData['Time'] == 2021)]
+# countryName = HoverData['points'][0]['hovertext']
+# df=mergedData[mergedData['Country_Name'] == 'countryName']
+
+# mergedScoresdetDfData
+
+categories = ["Economy_(GDP_per_Capita)", 
+              "Family", 
+              "Health_(Life_Expectancy)", 
+              "Freedom", 
+              "Trust_(Government_Corruption)", 
+              "Generosity", 
+              "Dystopia_Residual"
+              ]
+# values = mergedScoresdetDfData[mergedScoresdetDfData['Country_Name'] == "Finland"]
+
+# values = pd.DataFrame
+values = Y2021ScoresdetDf[Y2021ScoresdetDf['Country_Name'] == "Finland"]
+values = values[categories]
+
+# values = [1,1,1,1,1,1,0]
 
 # display(values)
+
+# happiness score detail visualization
+# @app.callback(
+#     Output('happiness-score-detail-polarchart', 'figure'),
+#     Input('happiness-score-scatter-line', 'hoverData'))
+# def update_graph(geoHoverData):
+#     countryName = geoHoverData['points'][0]['hovertext']
+#     df=mergedData[mergedData['Country_Name'] == countryName]
+#     fig =  px.scatter(df, 
+#                       x="Time", 
+#                       y="Happiness_Score",
+#                       hover_name="Country_Name" )
+#     fig.update_traces(mode='lines+markers')
+#     return fig
 
 # In[19]:
 
@@ -374,28 +516,28 @@ values = mergedData[(mergedData['Country_Name'] == "Finland")
 # values = [4, 7, 2, 5, 1, 6, 3]
 # colors = ['red', 'blue', 'green', 'orange', 'purple', 'cyan', 'magenta']
 
-# # Create traces for each category
-# traces = []
-# for i in range(len(categories)):
-#     trace = go.Barpolar(
-#         r=[values[i]],
-#         theta=[categories[i]],
-#         width=1,
-#         marker=dict(color=colors[i]),
-#         name=categories[i]
-#     )
-#     traces.append(trace)
+# Create traces for each category
+traces = []
+for i in range(len(categories)):
+    trace = go.Barpolar(
+        r=[values.iloc[:,i]],
+        theta=[categories[i]],
+        width=1,
+        marker=dict(color=px.colors.sequential.Plasma[i]),
+        name=categories[i]
+    )
+    traces.append(trace)
 
-# # Layout for the polar chart
-# layout = go.Layout(
-#     polar=dict(
-#         radialaxis=dict(range=[0, max(values)]),
-#     ),
-# )
+# Layout for the polar chart
+layout = go.Layout(
+    polar=dict(
+        radialaxis=dict(range=[0, max(values)]),
+    ),
+)
 
-# # Create the figure
-# fig2 = go.Figure(data=traces, layout=layout)
-# fig2.show()
+# Create the figure
+fig2 = go.Figure(data=traces, layout=layout)
+fig2.show()
 
 
 # I don't understand how callbacks works, but I wanted to show the detail of each subscore 
@@ -422,38 +564,38 @@ values = mergedData[(mergedData['Country_Name'] == "Finland")
 
 # Data for the polar chart
 # categories = ['Category 1', 'Category 2', 'Category 3', 'Category 4', 'Category 5', 'Category 6', 'Category 7']
-categories = ['Category 1', 'Category 2', 'Category 3', 'Category 4', 'Category 5', 'Category 6', 'Category 7']
-values = [4, 7, 2, 5, 1, 6, 3]
+# categories = ['Category 1', 'Category 2', 'Category 3', 'Category 4', 'Category 5', 'Category 6', 'Category 7']
+# values = [4, 7, 2, 5, 1, 6, 3]
 # values = mergedData[(mergedData['Country_Name'] == countryName)
 #                   & (mergedData['Time'] == year_value)]
 
-colors = ['red', 'blue', 'green', 'orange', 'purple', 'cyan', 'magenta']
+# colors = ['red', 'blue', 'green', 'orange', 'purple', 'cyan', 'magenta']
 
 # countryName = geoHoverData['points'][0]['hovertext']
 #     df=
 
-# Create traces for each category
-traces = []
-for i in range(len(categories)):
-    trace = go.Barpolar(
-        r=[values[i]],
-        theta=[categories[i]],
-        width=1,
-        marker=dict(color=colors[i]),
-        name=categories[i]
-    )
-    traces.append(trace)
+# # Create traces for each category
+# traces = []
+# for i in range(len(categories)):
+#     trace = go.Barpolar(
+#         r=[values[i]],
+#         theta=[categories[i]],
+#         width=1,
+#         marker=dict(color=colors[i]),
+#         name=categories[i]
+#     )
+#     traces.append(trace)
 
-# Layout for the polar chart
-layout = go.Layout(
-    polar=dict(
-        radialaxis=dict(range=[0, max(values)]),
-    ),
-)
+# # Layout for the polar chart
+# layout = go.Layout(
+#     polar=dict(
+#         radialaxis=dict(range=[0, max(values)]),
+#     ),
+# )
 
-# Create the figure
-fig2 = go.Figure(data=traces, layout=layout)
-fig2.show()
+# # Create the figure
+# fig2 = go.Figure(data=traces, layout=layout)
+# fig2.show()
 
 
 # In[ ]:
